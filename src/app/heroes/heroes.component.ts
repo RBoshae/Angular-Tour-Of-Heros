ff2de662981f171a9f2cfdd72307ce3c2defec70
Wanderrer <rwboshae@gmail.com>
2018-09-24 01:58:45 -0700

    Hide empty details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
+selectedHero: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
 heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   constructor() {
   }
